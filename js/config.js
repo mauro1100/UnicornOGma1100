a79294ccc3f0c8fd8c43f674a5705f7c90dc8320
mauro1100 <mauroalvarenga@my.unt.edu>
2024-04-21 12:17:29 -0500

    config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-east-2_RAtMzO4vj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'f10b45d0-50b170195de5c1ddcaaebec5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
